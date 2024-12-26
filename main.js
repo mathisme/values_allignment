d138440ab5795ce765bcdfd01621274b2d971f92
Anne Losch <anne.m.losch@gmail.com>
2024-12-26 13:52:29 -0600

    started working on js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const model = {};
+const model = {
+	valuesDiv : document.getElementById("values_list"),
+	submitButton : document.getElementById("submit"),
+	instructionSpan : document.getElementById("values_text"),
+	valuesInput : {
+		family : document.getElementById("family"),
+		friends : document.getElementById("friends"),
+		community : document.getElementById("community"),
+		environment : document.getElementById("environment"),
+		work : document.getElementById("work"),
+		edu : document.getElementById("edu"),
+		rec : document.getElementById("rec"),
+		spiritual : document.getElementById("spiritual"),
+		health : document.getElementById("health"),
+		partner : document.getElementById("partner")
+	}
+};
 const view = {};
-const controller = {};
+const controller = {
+	init : function(){}
+};
+
+controller.init();
